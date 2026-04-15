11bab79a856c3751fdda4a534dc47a0a23aea500
poojithasagar016-spec <poojithasagar016@gmail.com>
2026-04-15 14:43:21 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-
-function manipulatestyles(){
-document.getElementById("headingElement").textContent = "You will win...!!!We will win....!! Pooja is with you";
-document.getElementById("headingElement").style.color ="Green";
-		
-}
